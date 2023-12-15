18eb433f5ecf37a71e637b554b3acec88a0f4200
jkociecki <116763591+jkociecki@users.noreply.github.com>
2023-12-15 15:48:28 +0100

    mod07 fix

---
+++ labolatoria/lab07/mod07.ml
@@ -0,0 +1,32 @@
+let sievie n = 
+  if n < 2 then [||]
+  else
+    let start = Array.make (n + 1) 0 in 
+    for i = 2 to n do start.(i) <- i
+    done;
+
+    for i = 2 to n do 
+      if(start.(i) != 0) then
+        for j = i * i to n do
+          if(start.(j) mod i = 0) then start.(j) <- 0
+        done;
+    done;
+  
+    let result_size = ref 0 in
+    for i = 2  to n do 
+      if(start.(i) != 0) then result_size := !result_size + 1; 
+    done;
+  
+    let counter = ref 0 in
+    let result = Array.make !result_size 0 in 
+    for i = 2 to n do
+      if(start.(i) != 0) then 
+        (result.(!counter) <- start.(i); counter := !counter + 1)
+    done;
+    result;;
+
+sievie 100;;
+
+
+
+
